b9f3a61171c2b4a489bf3863bd2fe385452ea558
Yuri <yurigpaulosenai-cyber@users.noreply.github.com>
2026-09-16 11:47:07 -0300

    fix: vercel build error

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   root: './',
